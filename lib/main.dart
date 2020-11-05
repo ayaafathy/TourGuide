16062e4820584d18d1e27b75c0bd97cb633ae6f7
ayaafathy <iayafathy@gmail.com>
2020-11-06 01:51:17 +0200

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,161 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: 'Login Page',
+      home: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/citylights.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            elevation: 5,
+            backgroundColor: Colors.transparent,
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.lightBlue,
+                size: 35,
+              ),
+              //  tooltip: '',
+              onPressed: () {},
+            ),
+            title: Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.lightBlue,
+              ),
+            ),
+            centerTitle: true,
+          ),
+          body: SafeArea(
+            child: LoginForm(),
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+        padding: EdgeInsets.only(top: 20),
+        child: (Column(
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: buildTexField(Icons.person, 'Username', false),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: buildTexField(Icons.vpn_key, 'Password', true),
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 15, bottom: 2),
+              child: buildButton('Continue', Icons.email),
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {},
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SignInButton(
+                  Buttons.Facebook,
+                  mini: true,
+                  onPressed: () {},
+                ),
+                SignInButton(
+                  Buttons.Twitter,
+                  mini: true,
+                  onPressed: () {},
+                ),
+                SignInButton(
+                  Buttons.Apple,
+                  mini: true,
+                  onPressed: () {},
+                ),
+                /*
+                SignInButtonBuilder(
+                  icon: Icons.email,
+                  text: "Ignored for mini button",
+                  mini: true,
+                  onPressed: () {},
+                  backgroundColor: Colors.cyan,
+                ),
+                */
+              ],
+            ),
+            Padding(
+                padding: EdgeInsets.only(top: 70),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "DON'T HAVE AN ACCOUNT? ",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                        color: Colors.lightBlue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      'CREATE ONE!',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                        color: Colors.lightBlue,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                )),
           ],
+        )));
+  }
+
+  TextFormField buildTexField(IconData icon, String hint, bool obscure) {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          icon,
+          size: 35,
+          color: Colors.lightBlue,
         ),
+        hintText: hint,
+        //labelText: ''
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      obscureText: obscure,
+      //onSaved: (value){},
+      //validator: (value){},
+      //autocorrect: ,
+    );
+  }
+
+  SignInButtonBuilder buildButton(String text, IconData icon) {
+    return SignInButtonBuilder(
+      text: text,
+      icon: icon,
+      onPressed: () {},
+      backgroundColor: Colors.lightBlue,
     );
   }
 }
