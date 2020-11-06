d4c4cc36873f9f9041b5cbd9aab37ae28b21886a
ayaafathy <iayafathy@gmail.com>
2020-11-06 15:36:40 +0200

    Sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,37 @@ class MyApp extends StatelessWidget {
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/citylights.jpg'),
+            image: AssetImage('images/24hrs.jpg'),
             fit: BoxFit.cover,
           ),
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
+          resizeToAvoidBottomPadding: false,
           appBar: AppBar(
             elevation: 5,
             backgroundColor: Colors.transparent,
             leading: IconButton(
               icon: Icon(
                 Icons.arrow_back,
-                color: Colors.lightBlue,
+                color: Colors.white,
                 size: 35,
               ),
               //  tooltip: '',
               onPressed: () {},
             ),
             title: Text(
-              'Login',
+              'Sign in',
               style: TextStyle(
-                color: Colors.lightBlue,
+                color: Colors.white,
               ),
             ),
             centerTitle: true,
           ),
           body: SafeArea(
-            child: LoginForm(),
+            child: SingleChildScrollView(
+              child: BuildForm(),
+            ),
           ),
         ),
       ),
@@ -50,24 +53,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginForm extends StatelessWidget {
+class BuildForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.only(top: 20),
+        padding: EdgeInsets.only(left: 25, top: 80, right: 25),
         child: (Column(
-          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
+            Container(
+                padding: EdgeInsets.only(left: 5, top: 2, right: 5, bottom: 10),
+                color: Colors.white70,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: buildTexField(Icons.person, 'Username', false),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: buildTexField(Icons.vpn_key, 'Password', true),
+                    ),
+                  ],
+                )),
             Padding(
-              padding: EdgeInsets.all(25),
-              child: buildTexField(Icons.person, 'Username', false),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: buildTexField(Icons.vpn_key, 'Password', true),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15, bottom: 2),
+              padding: EdgeInsets.only(top: 40, bottom: 2),
               child: buildButton('Continue', Icons.email),
             ),
             SignInButton(
@@ -104,26 +114,26 @@ class LoginForm extends StatelessWidget {
               ],
             ),
             Padding(
-                padding: EdgeInsets.only(top: 70),
+                padding: EdgeInsets.only(top: 60, bottom: 20),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
                       "DON'T HAVE AN ACCOUNT? ",
                       textAlign: TextAlign.center,
-                      style: GoogleFonts.lato(
-                        color: Colors.lightBlue,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w700,
+                      style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                     Text(
                       'CREATE ONE!',
                       textAlign: TextAlign.center,
-                      style: GoogleFonts.lato(
-                        color: Colors.lightBlue,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w700,
+                      style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ],
@@ -137,12 +147,18 @@ class LoginForm extends StatelessWidget {
       decoration: InputDecoration(
         icon: Icon(
           icon,
-          size: 35,
+          size: 30,
           color: Colors.lightBlue,
         ),
         hintText: hint,
         //labelText: ''
       ),
+      style: GoogleFonts.roboto(
+        color: Colors.black,
+        fontSize: 17,
+        fontWeight: FontWeight.w500,
+      ),
+
       obscureText: obscure,
       //onSaved: (value){},
       //validator: (value){},
