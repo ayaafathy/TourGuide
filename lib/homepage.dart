758388f2bd4fd656ddfe9ab26a49edefdf37c068
Maarc01 <45775981+Maarc01@users.noreply.github.com>
2020-11-18 02:52:48 +0200

    Map but need small fixes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,6 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tour_guide/widgets/destination_carousel.dart';
 import 'package:tour_guide/widgets/hotel_carousel.dart';
 import 'dash_UI.dart';
+import 'map.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -66,6 +67,10 @@ class _HomeScreenState extends State<HomeScreen> {
           setState(() {
             _currentTab = value;
           });
+          if (value == 1){
+
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> Maps()));
+          }
         },
         items: [
           BottomNavigationBarItem(
@@ -80,6 +85,7 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: Icon(
               Icons.public,
               size: 30,
+
             ),
             // ignore: deprecated_member_use
             title: SizedBox.shrink(),
