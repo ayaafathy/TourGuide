6914590ecdbdcf02f51fa074005b34cbb421f616
ayaafathy <iayafathy@gmail.com>
2020-12-01 17:56:30 +0200

    help screen

--- lib/screens/faq_UI.dart
+++ lib/screens/faq_UI.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:expansion_tile_card/expansion_tile_card.dart';
-import 'package:tour_guide/widgets/appBar.dart';
 import 'package:tour_guide/widgets/fonts_style.dart';
-
-void main() => runApp(HelpCenter());
-
-class HelpCenter extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(
-        constraints: BoxConstraints.expand(),
-        child: Scaffold(
-          appBar: buildAppBar(null, () {}, 'Help'),
-          body: SafeArea(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
-              child: FAQ(),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//import 'package:tour_guide/models/faq_model.dart';
 
 class FAQ extends StatefulWidget {
-  FAQ({Key key}) : super(key: key);
+  //FAQ({Key key}) : super(key: key);
 
   @override
   _FAQState createState() => _FAQState();
@@ -35,13 +12,39 @@ class FAQ extends StatefulWidget {
 
 class _FAQState extends State<FAQ> {
   //final _cardKey = new GlobalKey<ExpansionTileCardState>();
-
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         faq(),
+        contactUs(),
       ],
+    );
+  }
+
+  Widget contactUs() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(top: 15, left: 50, right: 50),
+        padding: EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(102, 165, 255, 0.1),
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        child: ListTile(
+          leading: Icon(
+            Icons.contact_support_rounded,
+            size: 30,
+            color: Colors.grey[700],
+          ),
+          title: Text(
+            'Contact us',
+            textAlign: TextAlign.left,
+            style: tilesStyle(),
+          ),
+        ),
+      ),
     );
   }
 
@@ -49,10 +52,11 @@ class _FAQState extends State<FAQ> {
     return Container(
       padding: EdgeInsets.all(8),
       decoration: BoxDecoration(
-        color: Color.fromRGBO(102, 165, 255, 0.2),
+        color: Color.fromRGBO(102, 165, 255, 0.1),
         borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           ListTile(
             leading: Icon(
@@ -66,22 +70,24 @@ class _FAQState extends State<FAQ> {
               style: tilesStyle(),
             ),
           ),
-          faqCard('Question 1', 'Answer 1'),
+          faqCard(),
+          faqCard(),
         ],
       ),
     );
   }
 
-  Widget faqCard(String q, String a) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+  Widget faqCard() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
       child: ExpansionTileCard(
         //key: _cardKey,
         baseColor: Colors.white60,
         borderRadius: BorderRadius.all(Radius.circular(12)),
         elevation: 2.0,
         //leading: Icon(Icons.help_center_rounded),
-        title: Text(q),
+        title: Text(''),
         children: <Widget>[
           Align(
             alignment: Alignment.centerLeft,
@@ -91,7 +97,7 @@ class _FAQState extends State<FAQ> {
                 vertical: 8.0,
               ),
               child: Text(
-                a,
+                '',
                 style: tilesStyle(),
               ),
             ),
