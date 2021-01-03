2aa289c7c2271cdd523a2f3895feb4eaebf00472
ayaafathy <iayafathy@gmail.com>
2021-01-03 20:41:31 +0200

    updated user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,11 +3,11 @@ import 'package:http/http.dart' as http;
 class User {
   String imageUrl;
   String name;
+  String email;
+  String username;
+  String password;
 
-  User({
-    this.imageUrl,
-    this.name,
-  });
+  User({this.imageUrl, this.name, this.email, this.username, this.password});
 }
 
 final List<User> hotels = [
