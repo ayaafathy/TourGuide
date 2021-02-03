8f4b5b9f045e31b1cc0bed1a50e94298b7b671b0
ayaafathy <iayafathy@gmail.com>
2021-02-03 19:11:20 +0200

    Auto sign in and sign out

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -65,13 +65,26 @@ class Authentication with ChangeNotifier {
       _email = responseData['email'];
       _token = responseData['idToken'];
 
-      /*
+      /// For debugging
       if (_userID == null) {
         throw HttpException('Null ID');
       }
-      */
 
-      //_autoSignOut();
+      /// For debugging ///*****
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(
+            responseData['expiresIn'],
+          ),
+        ),
+      );
+      print('Auth, User id is : $_userID');
+      print('Auth, Token is : $_token');
+      print('Auth, _expiryDate is : $_expiryDate');
+
+      /// End of for debugging ///*****
+
+      _autoSignOut();
 
       /// Provider
       notifyListeners();
@@ -83,7 +96,7 @@ class Authentication with ChangeNotifier {
         'token': _token,
         'expiryDate': _expiryDate.toIso8601String(),
 
-        /// Standard format
+        /// Standard format: toIso8601String()
       });
 
       prefs.setString('TG_User', userData);
@@ -98,5 +111,62 @@ class Authentication with ChangeNotifier {
 
   Future<void> signIn(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  Future<void> signOut() async {
+    _userID = null;
+    _email = null;
+    _token = null;
+    _expiryDate = null;
+    _authenticationTimer?.cancel();
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  //***TODO: check this again
+  Future<bool> _autoSignIn() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (!prefs.containsKey('TG_User')) {
+      return false;
+    }
+
+    final savedUserData =
+        json.decode(prefs.getString('TG_User')) as Map<String, dynamic>;
+    _expiryDate = DateTime.parse(savedUserData['expiryDate']);
+
+    /// For debugging ///*****
+    if (_expiryDate.isBefore(DateTime.now())) {
+      print('Expired');
+      return false;
+    }
+    print("//Auto SignIn $savedUserData");
+
+    /// End of for debugging ///*****
+
+    try {
+      _userID = savedUserData['userID'];
+      _token = savedUserData['token'];
+      notifyListeners();
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+
+    /// For debugging ///*****
+    print('Test: $_token');
+    print('_expiryDate: $_expiryDate');
+
+    /// End of for debugging ///*****
+    return true;
+  }
+
+  Future<void> _autoSignOut() {
+    if (_authenticationTimer != null) {
+      _authenticationTimer.cancel();
+    }
+    _authenticationTimer =
+        Timer(_expiryDate.difference(DateTime.now()), signOut);
   }
 }
